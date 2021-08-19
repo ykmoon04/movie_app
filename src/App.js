a2d518e3374eeb41b14d518f0281445bbf83e1f9
ykmoon04 <moonsc014@gmail.com>
2021-08-19 09:32:22 +0900

    creating movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 function App() {
+  const [isLoading, changeState] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  //wait until getting api is done
+  async function getMovies() {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    setMovies(movies);
+    changeState(false);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => {
+            return (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            );
+          })}
+        </div>
+      )}
+    </section>
   );
 }
+
+// class App extends React.Component {
+//   state = {
+//     isLoading: true,
+//     movies: [],
+//   };
+
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ isLoading: false });
+//     }, 6000);
+//   }
+//   render() {
+//     const { isLoading } = this.state;
+//     return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+//   }
+// }
 
 export default App;
